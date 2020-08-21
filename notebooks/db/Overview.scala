58ffda66db790d17e1091f5ea6001ea62b2ed211
Raazesh Sainudiin <raazesh.sainudiin@gmail.com>
2020-08-21 19:27:12 +0200

    update

--- notebooks/db/Overview.scala
+++ notebooks/db/Overview.scala
@@ -1,25 +1,36 @@
 // Databricks notebook source
 // MAGIC %md
-// MAGIC # Events and Entities of Interest to OIL/GAS Trends
+// MAGIC # Spark GDELT Examples
+// MAGIC ## Detecting Events and Entities of Interest to OIL/GAS Price Trends
 // MAGIC 
 // MAGIC Johannes Graner, Albert Nilsson and Raazesh Sainudiin
 // MAGIC 
 // MAGIC 2020, Uppsala, Sweden
 // MAGIC 
-// MAGIC This work was inspired by Antoine Aamennd's texata-2017 repository forked here:
-// MAGIC 
-// MAGIC - https://github.com/lamastex/spark-texata-2020/
-// MAGIC 
-// MAGIC The following libraries were developed to detect events and entities (persons, organisations, etc.) in GDELT mass media intelligence metadata that may be of potential interest to trends and their reversals in oil prices.
-// MAGIC 
-// MAGIC - The spark-gdelt library was extended to be an exhaustive scraper of GDELT version 1 and 2 files
-// MAGIC - spark-trend-calculus library was written for structured streams
 // MAGIC 
 // MAGIC This project was supported by Combient Mix AB through summer internships at:
 // MAGIC 
 // MAGIC Combient Competence Centre for Data Engineering Sciences, 
 // MAGIC Department of Mathematics, 
 // MAGIC Uppsala University, Uppsala, Sweden
+// MAGIC 
+// MAGIC See Example notebooks to detect events and persons or entities of interest
+// MAGIC 
+// MAGIC - [notebooks/db/gdelt-EOI-detection](notebooks/db/gdelt-EOI-detection.md)
+// MAGIC - [notebooks/db/gdelt-POI-detection](notebooks/db/gdelt-POI-detection.md)
+// MAGIC 
+// MAGIC # Resources
+// MAGIC 
+// MAGIC This builds on the following libraries and its antecedents therein:
+// MAGIC 
+// MAGIC - [https://github.com/aamend/spark-gdelt](https://github.com/aamend/spark-gdelt) 
+// MAGIC - [https://github.com/lamastex/spark-trend-calculus](https://github.com/lamastex/spark-trend-calculus)
+// MAGIC 
+// MAGIC 
+// MAGIC ## This work was inspired by:
+// MAGIC 
+// MAGIC - Antoine Aamennd's [texata-2017](https://github.com/aamend/texata-r2-2017)
+// MAGIC - Andrew Morgan's [Trend Calculus Library](https://github.com/ByteSumoLtd/TrendCalculus-lua)
 
 // COMMAND ----------
 
